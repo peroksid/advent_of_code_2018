b37d033c1b0f1ea54177097779020ceb0a01f225
Alexander Pugachev <alexander@Alexanders-MacBook-Air.local>
2018-12-17 15:33:22 +0000

    starting day 3 on day 17

---
+++ d3t1/lib/d3t1.ex
@@ -0,0 +1,18 @@
+defmodule D3t1 do
+  @moduledoc """
+  Documentation for D3t1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> D3t1.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ d3t1/test/d3t1_test.exs
@@ -0,0 +1,8 @@
+defmodule D3t1Test do
+  use ExUnit.Case
+  doctest D3t1
+
+  test "greets the world" do
+    assert D3t1.hello() == :world
+  end
+end
